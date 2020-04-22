246aba3b15ddabc97624217d6fcb5a7fd6e81a08
lefevre <jean-paul.lefevre@cea.fr>
2020-04-22 16:06:53 +0200

    Preparing the player commands

--- bin/player.sh
+++ bin/player.sh
@@ -1,2 +1,34 @@
 #!/bin/sh
-onair.sh -t 2 a-fip
+#______________________________________________________________________________
+
+# Script used launch and control the mpg123 player.
+# Jean-Paul Le Fèvre - April 2020
+
+# Usage : player.sh
+# It can be controlled by sending command in the fifo
+# The working directory is $RADIOG_HOME/run
+#______________________________________________________________________________
+
+if [ -z "$RADIOG_HOME" ]; then
+    echo "export RADIOG_HOME="
+    exit 1
+fi
+
+rundir=$RADIOG_HOME/run
+fifo=$rundir/mpgctrl
+pidfile=$rundir/mpg.pid
+
+if [ -f $pidfile ]; then
+    echo "A player may be still running. Kill it."
+    echo "The pid is : " `cat $pidfile`
+    exit 1
+fi
+
+
+echo "Launching the radiog player ..."
+echo /usr/bin/mpg123 -R --fifo $fifo 1>/dev/null &
+
+echo $! > $pidfile
+echo "The radiog player is " `cat $pidfile`
+
+#______________________________________________________________________________

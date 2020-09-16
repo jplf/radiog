7f55156d23e0235d3c8981b6703ffb196af81caf
lefevre <jean-paul.lefevre@cea.fr>
2020-09-16 18:09:50 +0200

    First unit tests implemented

--- backend/src/player/player.controller.spec.ts
+++ backend/src/player/player.controller.spec.ts
@@ -1,12 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PlayerController } from './player.controller';
+import { PlayerService } from './player.service';
+import { StationsService } from '../stations/stations.service';
+import { ConfigService } from '@nestjs/config';
+import { Journal } from '../journal/journal.service';
 
 describe('Player Controller', () => {
   let controller: PlayerController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [PlayerController],
+        controllers: [PlayerController],
+        providers: [StationsService, PlayerService, ConfigService, Journal]
     }).compile();
 
     controller = module.get<PlayerController>(PlayerController);
--- backend/src/stations/stations.service.spec.ts
+++ backend/src/stations/stations.service.spec.ts
@@ -1,18 +1,44 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { ConfigModule } from '@nestjs/config';
 import { StationsService } from './stations.service';
+import { ConfigService } from '@nestjs/config';
+import { Journal } from '../journal/journal.service';
+import { Station } from './station.interface';
 
 describe('StationsService', () => {
-  let service: StationsService;
+    let service: StationsService;
+    let config: ConfigService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [StationsService],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            imports: [
+                ConfigModule.forRoot({
+                    isGlobal: true,
+                    envFilePath: process.env.RADIOG_CONF
+                })
+            ],
+            providers: [StationsService, ConfigService, Journal]
+        }).compile();
 
-    service = module.get<StationsService>(StationsService);
-  });
+        service = module.get<StationsService>(StationsService);
+        config  = module.get<ConfigService>(ConfigService);
+    });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+    it('should be defined', () => {
+        expect(service).toBeDefined();
+    });
+    
+    it('path to stations should be valid', () => {
+        var path = config.get<string>('RADIOG_HOME') + '/'
+            + config.get<string>('STATION_LIST');
+        
+        expect(path).toMatch(/stations.json/);
+    });
+    
+    it('stations are loaded', () => {
+        return service.load().then(list => {
+            expect(list.length).toBeGreaterThan(1);
+        });
+    });
+    
 });

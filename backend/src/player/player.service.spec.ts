3b4745673d050ff2a8c29829fafe8d8b759d4f40
lefevre <jean-paul.lefevre@cea.fr>
2020-09-30 14:47:47 +0200

    Test added

--- backend/src/player/player.service.spec.ts
+++ backend/src/player/player.service.spec.ts
@@ -1,18 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { ConfigService } from '@nestjs/config';
 import { PlayerService } from './player.service';
+import { Player } from './player.interface';
+import { Journal } from '../journal/journal.service';
 
 describe('PlayerService', () => {
-  let service: PlayerService;
+    let service: PlayerService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [PlayerService],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [PlayerService, ConfigService, Journal],
+        }).compile();
 
-    service = module.get<PlayerService>(PlayerService);
-  });
+        service = module.get<PlayerService>(PlayerService);
+    });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+    it('player service should be defined', () => {
+        expect(service).toBeDefined();
+    });
+    
+    it('player should be defined', () => {
+        var player = service.getPlayer();
+        expect(player).toBeDefined();
+    });
+    
 });

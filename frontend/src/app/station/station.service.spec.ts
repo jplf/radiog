3a832d63fddf5ff16a02c8798bde0feffc8dd8f9
lefevre <jean-paul.lefevre@cea.fr>
2020-10-14 16:16:33 +0200

    List of providers at last fixed

--- frontend/src/app/station/station.service.spec.ts
+++ frontend/src/app/station/station.service.spec.ts
@@ -1,16 +1,23 @@
 import { TestBed } from '@angular/core/testing';
-
 import { StationService } from './station.service';
+import { DatePipe } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { HttpHandler } from '@angular/common/http';
 
 describe('StationService', () => {
-  let service: StationService;
+    
+    let service: StationService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(StationService);
-  });
+    beforeEach(async () => {
+        TestBed.configureTestingModule({
+            
+            providers: [StationService, DatePipe, HttpClient, HttpHandler]
+        });
+        
+        service = TestBed.inject(StationService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 });

13cbb586ac25a18d30877e55896e506d8b9e7551
lefevre <jean-paul.lefevre@cea.fr>
2021-12-16 19:45:28 +0100

    More messages printed

--- bin/start.sh
+++ bin/start.sh
@@ -55,7 +55,11 @@ sleep 30
 echo "Backend server is now accepting requests !" | tee -a ../run/backend.log
 curl -s $RADIOG_URL/player | jq
 curl -s $RADIOG_URL/output/info | jq
-echo "Try something like : curl -s $RADIOG_URL/player/listen/10"
+
+echo "Try something like :"
+echo "curl -s $RADIOG_URL/player/set?volume=30"
+echo "curl -s $RADIOG_URL/player/on"
+echo "curl -s $RADIOG_URL/player/listen/10"
 
 # The frontend server is managed by nginx.
 cd $RADIOG_HOME/run

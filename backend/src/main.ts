de360ff6b8d4a15f13a2fc6c621e6e637158bdb5
lefevre <jean-paul.lefevre@cea.fr>
2022-01-21 16:54:52 +0100

    Location of certificates updated

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -12,16 +12,27 @@ import { Journal } from './journal/journal.service';
 import { StationsService } from './stations/stations.service';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
+import * as fs from 'fs';
+
 /**
- * curl http://localhost:3000/player|jq
- * curl http://localhost:3000/device/info| jq
- * curl http://localhost:3000/device/connect
- * curl http://localhost:3000/player/listen/10
- * curl http://localhost:3000/player/set?volume=30
+ * curl -k http://localhost:3000/player|jq
+ * curl -k http://localhost:3000/device/info| jq
+ * curl -k http://localhost:3000/device/connect
+ * curl -k http://localhost:3000/player/listen/10
+ * curl -k http://localhost:3000/player/set?volume=30
  */
 async function bootstrap() {
 
+    // Something like /home/lefevre/work/hub/radiog/etc/madeux
+    const certPath = process.env.RADIOG_HOME + '/etc/' + process.env.HOSTNAME;
+          
+    const httpsOptions = {
+        key: fs.readFileSync(certPath + '.key.pem'),
+        cert: fs.readFileSync(certPath + '.cert.pem'),
+    };
+    
     const app = await NestFactory.create(AppModule, {
+        httpsOptions,
         bufferLogs: true
     });
 
@@ -40,6 +51,7 @@ async function bootstrap() {
     app.useLogger(journal);
 
     journal.log('Application is about to start');
+    journal.log('Certificates are like ' + certPath);
 
     // This service retrieves the list of known radios
     // app.get(StationsService);

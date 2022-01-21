aa7a86765d346486d6372878e5e0aa26e3f9974a
lefevre <jean-paul.lefevre@cea.fr>
2022-01-21 16:53:09 +0100

    Typo on curlopt fixed

--- bin/start.sh
+++ bin/start.sh
@@ -54,13 +54,13 @@ sleep 30
 
 echo "Backend server is now accepting requests !" | tee -a ../run/backend.log
 curlopt="-sk"
-curl $curlop $RADIOG_URL/player | jq
-curl $curlop $RADIOG_URL/output/info | jq
+curl $curlopt $RADIOG_URL/player | jq
+curl $curlopt $RADIOG_URL/output/info | jq
 
 echo "Try something like :"
-echo "curl $curlop $RADIOG_URL/player/set?volume=30"
-echo "curl $curlop $RADIOG_URL/player/on"
-echo "curl $curlop $RADIOG_URL/player/listen/10"
+echo "curl $curlopt $RADIOG_URL/player/set?volume=30"
+echo "curl $curlopt $RADIOG_URL/player/on"
+echo "curl $curlopt $RADIOG_URL/player/listen/10"
 
 # The frontend server is managed by nginx.
 cd $RADIOG_HOME/run

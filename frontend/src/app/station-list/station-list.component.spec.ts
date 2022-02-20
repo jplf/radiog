be1fc3e40fad2f200dc2fdaffcb4fca2b7e47744
lefevre <jean-paul.lefevre@cea.fr>
2022-02-20 18:09:09 +0100

    Trying to update

--- frontend/src/app/station-list/station-list.component.spec.ts
+++ frontend/src/app/station-list/station-list.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { StationListComponent } from './station-list.component';
 
@@ -6,7 +6,7 @@ describe('StationListComponent', () => {
   let component: StationListComponent;
   let fixture: ComponentFixture<StationListComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ StationListComponent ]
     })
--- frontend/src/app/station/station.component.spec.ts
+++ frontend/src/app/station/station.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { StationComponent } from './station.component';
 
@@ -6,7 +6,7 @@ describe('StationComponent', () => {
   let component: StationComponent;
   let fixture: ComponentFixture<StationComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ StationComponent ]
     })
